f754df8ecf47b9fe0f0dcb84ee86bb7c3f1aa1e8
AbraaoMorais <76604710+AbraaoMorais@users.noreply.github.com>
2025-09-01 23:09:38 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./*.html"], // ou "./src/**/*.{html,js}" se estiver usando pasta src
+    theme: {
+      extend: {
+        colors: {
+          midnight: 'rgb(9 13 40)',
+      },
+    },
+    },
+    plugins: [],
+  }
